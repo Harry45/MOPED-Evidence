847fc7547871bb5401fd8e8622f7672666ae0859
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2022-07-06 18:12:35 +0400

    notebook

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from src.nested import BayesFactor
 import config as CONFIG
 
 TEST = BayesFactor(0.2, CONFIG.NDATA)
-results = TEST.summaries(5, verbose=True, save=True)
+results = TEST.summaries(5000, verbose=False, save=True)
